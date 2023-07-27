6f819a4b554f995dd60f87ced89cf310eec69853
Thomas Hanson <thomashansondev@gmail.com>
2023-07-27 15:59:56 -0400

    Fixed type error preventing successful deployment.

--- src/pages/api/sendMessage.tsx
+++ src/pages/api/sendMessage.tsx
@@ -1,12 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import twilio from 'twilio';
 
+interface RequestBody {
+  phone: string;
+  message: string;
+}
+
 export default function sendMessage(req: NextApiRequest, res: NextApiResponse) {
   const twilioNumber = process.env.TWILIO_PHONE_NUMBER
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const token = process.env.TWILIO_AUTH_TOKEN;
   const client = twilio(accountSid, token);
-  const { phone, message } = req.body;
+  const { phone, message } = req.body as RequestBody;
   // console.log(phone, message);
   client.messages
     .create({
