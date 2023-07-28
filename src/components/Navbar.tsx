6de5f989ab6871115536a7bb5dd3fafd70bda738
Thomas Hanson <thomashansondev@gmail.com>
2023-07-28 14:11:05 -0400

    Updated positioning of light/dark mode toggle and navigation items in different screen sizes.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,6 +35,21 @@ const Navbar = () => {
 
           <div className="flex items-center space-x-4 md:hidden">
             {/* Hamburger menu */}
+            <Switch
+              checked={theme === 'dark'}
+              onChange={toggleTheme}
+              className={`${
+                theme === 'dark' ? 'bg-green-600' : 'bg-gray-600'
+              } relative inline-flex flex-shrink-0 h-5 w-11 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span className="sr-only">Toggle dark mode</span>
+              <span
+                aria-hidden="true"
+                className={`${
+                  theme === 'dark' ? 'translate-x-6' : 'translate-x-0'
+                } pointer-events-none inline-block h-4 w-4 rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              />
+            </Switch>
             <button
               type="button"
               className={`text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900 ${
@@ -55,7 +70,7 @@ const Navbar = () => {
             <NavItem href="/uses">Uses</NavItem>
           </div>
 
-          <div>
+          <div className={`hidden md:flex md:items-center md:space-x-4 ${isMenuOpen ? 'block' : 'hidden'}`}>
             <Switch
               checked={theme === 'dark'}
               onChange={toggleTheme}
